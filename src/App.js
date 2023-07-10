e70970b91e7c3d51749d28ad97eae29a373429a6
Djibril-Mugisho <mugishodjibril2004@gmail>
2023-07-10 15:39:25 +0300

    fix linters errors

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <h1>Basic calculator</h1>
     </div>
   );
 }
